0d5a4e48f1d07f4b09ad7960c04984c309605090
fawwaz <maydafawwaz@gmail.com>
2020-07-25 00:37:08 +0800

    able to save journal but still some bugs

---
+++ MozBeauty/SkinJournal/JournalTableCell.swift
@@ -0,0 +1,38 @@
+//
+//  JournalTableCell.swift
+//  MozBeauty
+//
+//  Created by Muhammad Fawwaz Mayda on 24/07/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class JournalTableCell: UITableViewCell {
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var journalImageView: UIImageView!
+    static let identifier = "JournalCell"
+    @IBOutlet weak var descJournalLabel: UILabel!
+    @IBOutlet weak var headJournalLabel: UILabel!
+    @IBOutlet weak var imageContainer: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        print("FROM XIB")
+        style()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func style() {
+        self.imageContainer.layer.cornerRadius = CGFloat(imageContainer.bounds.width) / 2.0
+        
+        self.containerView.layer.cornerRadius = 15.0
+        
+    }
+    
+}
--- MozBeauty/SkinJournal/SkinJournalThird.swift
+++ MozBeauty/SkinJournal/SkinJournalThird.swift
@@ -16,6 +16,7 @@ class SkinJournalThird: UIViewController, UIGestureRecognizerDelegate, UINavigat
     @IBOutlet weak var wrinkleLabel: UILabel!
     var tapGesture: UITapGestureRecognizer?
     
+    var journalModel: Journal? 
     var ageModel = AgeModel()
     var visionModel = NetReq()
 
@@ -24,6 +25,9 @@ class SkinJournalThird: UIViewController, UIGestureRecognizerDelegate, UINavigat
         addGesture()
         visionModel.delegate = self
         ageModel.delegate = self
+        if journalModel == nil {
+            journalModel = Journal(context: ViewModel.globalContext)
+        }
     }
     
     func addGesture() {
@@ -45,6 +49,23 @@ class SkinJournalThird: UIViewController, UIGestureRecognizerDelegate, UINavigat
         picker.delegate = self
         present(picker, animated: true,completion: nil)
     }
+    
+    func updateUI() {
+        if let currentModel = journalModel {
+            DispatchQueue.main.async {
+                   self.imageView.image = UIImage(data: currentModel.photo!)
+                         self.acneLabel.text = "Acne Score: \(currentModel.acne)"
+                         self.wrinkleLabel.text = "Wrinkle Score: \(currentModel.foreheadwrinkle)"
+                self.skinAgeLabel.text = "Skin age: \(currentModel.skinage!)"
+            }
+        }
+    }
+    @IBAction func doneTapped(_ sender: Any) {
+        if let currentModel = journalModel {
+            currentModel.save()
+            dismiss(animated: true, completion: nil)
+        }
+    }
 }
 
 extension SkinJournalThird: UIImagePickerControllerDelegate {
@@ -52,28 +73,29 @@ extension SkinJournalThird: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let img = info[.originalImage] as? UIImage else {fatalError("Cant obtain Images")}
         
-        imageView.image = img
+        //imageView.image = img
+        journalModel?.photo = img.jpegData(compressionQuality: 0.8)!
         guard let cgImage = CIImage(image: img) else {
                   fatalError("Cant convert to CGIMage")
         }
         ageModel.detectAge(image: cgImage)
         visionModel.doRequestsAlamo(withImage: img)
         dismiss(animated: true)
+        self.imageView.removeGestureRecognizer(tapGesture!)
     }
 }
 
 extension SkinJournalThird: FaceServiceDelegate {
     func didGetAgePrediction(_ string: String) {
-        DispatchQueue.main.async {
-            self.skinAgeLabel.text = "Skin Age: \(string)"
-        }
+        journalModel?.skinage = string
+        self.updateUI()
     }
     
     func didGetSkinPrediction(_ skinResult: FaceResult) {
-        DispatchQueue.main.async {
-            self.acneLabel.text = "Acne Score: \(skinResult.result.acne.confidence)"
-            self.wrinkleLabel.text = "Wrinkle Score: \(skinResult.result.forehead_wrinkle.confidence)"
-        }
+        journalModel?.acne = skinResult.result.acne.confidence
+        journalModel?.foreheadwrinkle = skinResult.result.forehead_wrinkle.confidence
+        self.updateUI()
+       
     }
     
     
